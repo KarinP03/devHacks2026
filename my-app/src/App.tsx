273583bee7784ad9b91f33adc94757a0a48d17ce
namcartoon06 <hoangnam.ams06@gmail.com>
2026-02-20 17:34:15 -0600

    fixed background, added sprite, added button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,13 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import backgroundImage from "../../2d-assets/background/background3.png";
+import sprite from "../../2d-assets/sprite/walk2.png";
+import "./App.css";
 
 function App() {
-  const bgStyle: React.CSSProperties = {
-    backgroundImage: `url(${backgroundImage})`,
-    height: "100vh",
-    width: "200vh",
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-  };
-
   return (
     <>
-      <div className="gallery">
-        <img src={backgroundImage} className="background"></img>
+      <div>
+        <img src={sprite} className="sprite"></img>
+        <button className="button">+</button>
       </div>
     </>
   );
